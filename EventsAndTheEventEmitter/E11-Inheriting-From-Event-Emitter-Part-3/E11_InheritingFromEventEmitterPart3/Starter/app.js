ba7104fa4c57d99ac65bbe7301a8d73eb2e2f6ed
vladovoev <vladovoev@yahoo.co.uk>
2018-04-20 16:33:47 +0300

    es6

--- EventsAndTheEventEmitter/E11-Inheriting-From-Event-Emitter-Part-3/E11_InheritingFromEventEmitterPart3/Starter/app.js
+++ EventsAndTheEventEmitter/E11-Inheriting-From-Event-Emitter-Part-3/E11_InheritingFromEventEmitterPart3/Starter/app.js
@@ -1,21 +1,10 @@
-var EventEmitter = require('events');
-var util = require('util');
+'use strict';
 
-function Greetr() {
-	EventEmitter.call(this);
-	this.greeting = 'Hello world!';
-}
-
-util.inherits(Greetr, EventEmitter);
-
-Greetr.prototype.greet = function(data) {
-	console.log(this.greeting + ': ' + data);
-	this.emit('greet', data);
-}
+var Greetr = require('./greetr');
 
 var greeter1 = new Greetr();
 
-greeter1.on('greet', function(data) {
+greeter1.on('greet', function (data) {
 	console.log('Someone greeted!: ' + data);
 });
 
